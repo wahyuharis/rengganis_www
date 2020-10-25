52ad5abff98915b2760d4b87376450321e6ffad5
harits wahyu <harits.wahyu.hid@gmail.com>
2020-10-26 01:55:36 +0700

    trans print

--- page/pesan/detail.js
+++ page/pesan/detail.js
@@ -1,16 +1,4 @@
-function Printer_model() {
-    var self = this;
-
-    self.printer_list = ko.observableArray([
-        {'address': '123123123', 'name': 'rpp-02'},
-        {'address': '123123123', 'name': 'rpp-03'},
-        {'address': '123123123', 'name': 'rpp-04'},
-        {'address': '123123123', 'name': 'rpp-05'},
-    ]);
-}
-
 $(document).ready(function () {
-    ko.applyBindings(new Printer_model(), document.getElementById("detail_page"));
 
     var hostname_other = localStorage.getItem('hostname_other');
 
@@ -27,10 +15,57 @@ $(document).ready(function () {
         foto_usaha = hostname_other + 'logo_usaha/' + data_usaha.logo;
     }
 
-
     $('#logo_usaha').attr('src', foto_usaha);
 
     get_penjualan();
+
+    $('#print').click(function () {
+        $('#print-modal').modal('show');
+        list_device();
+    });
+
+    function list_device() {
+
+        window.DatecsPrinter.listBluetoothDevices(
+                function (devices) {
+                    var append = '';
+                    $('#printer-list').html('');
+
+                    for (var i = 0; i < devices.length; i++) {
+                        append = push_radio(devices[i].address, devices[i].name, i);
+                        $('#printer-list').append(append);
+                    }
+                    change_printer_handler();
+                },
+                function (error) {
+                    $('#console').append(JSON.stringify(error)) + "\n";
+                }
+        );
+    }
+
+    function push_radio(address, name, index) {
+        var html = '<div class="form-check mb-2 mt-2 border-top">'
+                + '<div class="mb-2 mt-2"  >'
+                + '<input class="form-check-input" type="radio" value="' + address + '" id="' + 'printer_' + index + '" name="printer_def"   >'
+                + '<label class="form-check-label" style="width:100%" for="' + 'printer_' + index + '" >'
+                + name
+                + '</label>'
+                + '</div>'
+                + '</div>';
+
+        return html;
+    }
+
+    function change_printer_handler() {
+        $('input[name=printer_def]').change(function () {
+            var mac_printer = $(this).val();
+            
+            localStorage.setItem('default_printer',mac_printer);
+            
+            $('#default_printer').val(mac_printer);
+        });
+    }
+
 
     $('#kembali').click(function () {
         localStorage.setItem('cart', '');
